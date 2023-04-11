1a248306bbcd7bb1c3b8e1b3859f53a587a297a7
Joao Mota <joaoefmota@gmail.com>
2023-04-11 12:48:46 +0200

    make it better

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const database = mysql.createPool({
     host: process.env.DB_HOST, 
-    port: parseInt(process.env.DB_PORT!), 
+    port: parseInt(process.env.PORT!), 
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
