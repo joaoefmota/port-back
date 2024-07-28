9e6c9728f3a1bb82d9826931e44b5c4aec6af875
João Mota <joaoefmota@gmail.com>
2024-07-28 13:12:29 +0200

    corrected variables

--- src/database.ts
+++ src/database.ts
@@ -4,12 +4,20 @@ import mysql from "mysql2/promise";
 dotenv.config();
 
 const database = mysql.createPool({
-    host: process.env.DB_HOST, 
-    port: parseInt(process.env.PORT!), 
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
-  
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.PORT!) || 5005,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+database.getConnection().then((connection) => {
+  console.log("Database connected!");
+  if (connection) {
+    connection.release();
+  } else {
+    console.error("Database connection failed");
+  }
+});
 
   export default database;
